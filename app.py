cb71614365c4ba6551c34400edabe15642026565
alessandroibes <alessandro.ibes@gmail.com>
2025-04-28 19:51:55 -0300

    Add log message

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
 
 @socketio.on('message')
 def handle_message(msg):
+    print(f'Mensagem recebida via chat: {msg}')
     emit('message', msg, broadcast=True)
 
 
