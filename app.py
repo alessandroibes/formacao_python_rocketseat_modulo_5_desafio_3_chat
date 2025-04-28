cc7e665b5cb1122c6c1bdb5a1ff74737f0225610
alessandroibes <alessandro.ibes@gmail.com>
2025-04-28 19:14:44 -0300

    Add templates/index.html file

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 
 app = Flask(__name__)
 
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def index():
-    return "Chat App"
+    return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(msg):
+    emit('message', msg, broadcast=True)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
